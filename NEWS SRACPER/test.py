355eaa757834787f08d944b67b71af82c099a0a0
Rishi Rami <88264686+error-raga-008@users.noreply.github.com>
2025-03-26 22:16:29 +0530

    Delete NEWS SRACPER/test.py

--- NEWS SRACPER/test.py
+++
@@ -1,5 +0,0 @@
-from transformers import BertTokenizer
-
-tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-
-tokenized_news = [tokenizer(news, padding="max_length", truncation=True, return_tensors="pt") for news in news_headlines]
